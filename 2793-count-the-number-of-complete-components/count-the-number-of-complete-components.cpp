d7ee055c70a22ae1169087c0b67256561bde7db3
VISHAL KUMAR <103324753+vk28122812@users.noreply.github.com>
2025-03-28 22:24:46 +0530

    Time: 80 ms (28.16%) | Memory: 137.9 MB (35.44%) - LeetSync

---
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>>&adj,vector<int>&vis,vector<int>&cmp,int u){
+        vis[u] = 1;
+        cmp.push_back(u);
+        for(int v:adj[u]){
+            if(vis[v])continue;
+            dfs(adj,vis,cmp,v);
+        }
+    }
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        vector<int> degree(n,0), vis(n,0);
+        vector<vector<int>>adj(n);
+        for(auto e:edges){
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+            degree[e[0]]++;
+            degree[e[1]]++;
+        }
+        int ans = 0 ;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                vector<int>cmp;
+                dfs(adj,vis,cmp,i);
+                bool flag = 1;
+                for(int c:cmp){
+                    if(degree[c] != (cmp.size()-1)){
+                        flag = 0;
+                        break;
+                    }
+                }
+                if(flag){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
