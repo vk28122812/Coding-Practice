51b94ed35d1da4549de91f75addb0a6cd6eda808
VISHAL KUMAR <103324753+vk28122812@users.noreply.github.com>
2025-04-04 20:57:00 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (87.17%) - LeetSync

--- 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
@@ -26,45 +26,20 @@ public:
         getDeepestLeaves(root->left,v,d+1,ht);
         getDeepestLeaves(root->right,v,d+1,ht);
     }
-    void getPath(Node*root,Node*target,vector<Node*>&path,bool&found){
-        if(found || !root)return;
-        if(root==target){
-            found = 1;
-            return;
-        }
-        path.push_back(root);
-        getPath(root->left,target,path,found);
-        getPath(root->right,target,path,found);
-        if(!found)path.pop_back();
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root)return 0;
+        if(root==p || root==q)return root;
+        TreeNode* left = lowestCommonAncestor(root->left,p,q);
+        TreeNode* right = lowestCommonAncestor(root->right,p,q);
+        if(left && right)return root;
+        else if(left)return left;
+        else return right;
     }
     TreeNode* lcaDeepestLeaves(TreeNode* root) {
         if(!root)return root;
         int ht = height(root);
         vector<Node*> v;
         getDeepestLeaves(root,v,1,ht);
-        if(v.size()==1){
-            return v.front();
-        }
-        vector<vector<Node*>>paths;
-        for(Node* node: v){
-            bool found = 0;
-            vector<Node*> p;
-            getPath(root,node,p,found);
-            paths.push_back(p);
-        }
-        Node* ans = 0;
-        for(int i=0;i<ht-1;i++){
-            Node* node = paths[0][i];
-            bool flag = 1;
-            for(int j=1;j<paths.size();j++){
-                if(paths[j][i] != node){
-                    flag = 0;
-                    break;
-                }
-            }
-            if(!flag)break;
-            ans = node;
-        }
-        return ans;
+        return lowestCommonAncestor(root,v.front(),v.back());
     }
 };
