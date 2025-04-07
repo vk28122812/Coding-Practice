fa0694cd7b3e477981255d495a71fcae728c6fc6
VISHAL KUMAR <103324753+vk28122812@users.noreply.github.com>
2025-04-07 15:32:45 +0530

    Time: 11 ms (42.53%) | Memory: 16.3 MB (19.08%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -4,30 +4,39 @@ public:
     vector<pair<int,int>> dir{
         {1,0}, {0,1}, {-1,0}, {0,-1}
     };
+    bool isPossible(vector<vector<int>>&grid,int mid){
+        if(grid[0][0] > mid)return 0;
+        queue<pair<int,int>>q;
+        int  m= grid.size(), n= grid[0].size();
+        vector<vector<int>>vis(m,vector<int>(n));
+        q.push({0,0});
+        vis[0][0] = 1;
+        while(q.size()){
+            int i = q.front().first;
+            int j = q.front().second;
+            q.pop();
+            if(i==m-1 && j==n-1)return 1;
+            for(auto&d:dir){
+                int x = i + d.first;
+                int y = j + d.second;
+                if(x>=0 && x < m && y >= 0 && y < n && !vis[x][y] &&grid[x][y]<=mid){
+                    q.push({x,y});
+                    vis[x][y] = 1;
+                }
+            }
+        }
+        return 0;
+    }
     int swimInWater(vector<vector<int>>& grid) {
         int m = grid.size(),n = grid[0].size();
         vector<vector<int>>vis(m, vector<int>(n,0));
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
-        pq.push({grid[0][0], 0,0,});
-        while(pq.size()){
-            int w = pq.top()[0];
-            int x = pq.top()[1];
-            int y = pq.top()[2];
-            pq.pop();
-            if(x==m-1 && y==n-1){
-                return w;
-            }
-            if(vis[x][y])continue;
-            vis[x][y] = 1;
-
-            for(auto&d:dir){
-                int i = x + d.first;
-                int j = y + d.second;
-                if(i < 0 || j < 0 || i==m || j==n )continue;
-                pq.push({max(w,grid[i][j]), i,j});
-
-            }
+        int low = 0, high = 2501;
+        while(low <= high){
+            int mid = (low+high)>>1;
+            if(isPossible(grid,mid)){
+                high = mid-1;
+            }else low = mid+1;
         }
-        return -1;
+        return low;
     }
 };
