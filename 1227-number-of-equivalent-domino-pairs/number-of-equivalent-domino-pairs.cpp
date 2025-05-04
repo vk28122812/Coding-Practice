8008879ac1f68c4c2ffeeebffa035c98a3f4e45a
VISHAL KUMAR <103324753+vk28122812@users.noreply.github.com>
2025-05-04 21:03:30 +0530

    Time: 3 ms (58.50%) | Memory: 26.3 MB (48.50%) - LeetSync

--- 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
@@ -1,24 +1,21 @@
+struct pair_hash{
+    size_t operator()(const pair<int,int>&p) const{
+        return hash<int>()(p.first) ^ hash<int>()(p.second << 1);
+    }
+};
 class Solution {
 public:
-  
     int numEquivDominoPairs(vector<vector<int>>& input) {
         int ans = 0;
         int n = input.size();
-        for(int i=0; i<n; i++){
-            if(input[i][0] > input[i][1]){
-                swap(input[i][0],input[i][1]);
-            }
+        unordered_map<pair<int,int>,int,pair_hash>mp;
+        for(auto&i:input){
+            mp[{min(i[0],i[1]), max(i[0],i[1])}] += 1;
         }
-        sort(input.begin(),input.end());
-        int l=0;
-        while(l<n){
-            int r=l+1;
-            while(r<n && input[l]==input[r]){
-                r++;
+        for(auto&i:mp){
+            if(i.second > 1){
+                ans += (i.second) * (i.second-1) / 2;
             }
-            int p = r-l;
-            ans += (p)*(p-1)/2;
-            l = r;
         }
         return ans;
     }
