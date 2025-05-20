4895636f4a62f32320011870e52a188081e9415a
VISHAL KUMAR <103324753+vk28122812@users.noreply.github.com>
2025-05-20 20:19:14 +0530

    Time: 1647 ms (5.03%) | Memory: 292.9 MB (71.03%) - LeetSync

---
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+        vector<int>pre(n+1,0);
+        for(auto&q:queries){
+            pre[q[0]]-=1;
+            pre[q[1]+1]+=1;
+        }   
+        for(auto i:nums)cout<<i<<" ";cout<<endl;
+        for(auto i:pre)cout<<i<<" ";
+        int s=0;
+        for(int i=0;i<n;i++){
+            s += pre[i];
+            if( nums[i]+s>0 )return 0;
+        }
+        return 1;
+
+        
+    }
+};
